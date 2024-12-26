d8d7eb949c08cfcd8927eb5745489ac95dae05c5
homelander143 <vaibhavdlalasangi@gmail.com>
2024-12-26 21:05:29 +0530

    Delete web_lab directory

--- web_lab/first.php
+++
@@ -1,8 +0,0 @@
-<?php 
-    print("<h1>Welcome to PHP</h1>"); 
-    $cookie_name = "user"; 
-    $cookie_value = "Vaibhav D Lalasangi"; 
-    setcookie($cookie_name, $cookie_value, time() + 86000); 
-    print "$cookie_name<br>"; 
-    print $_COOKIE[$cookie_name]; 
-?>
